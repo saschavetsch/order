2aeeadc67fd585d37e9070a95a522f34275200f7
nom <noeddi2be@users.noreply.github.com>
2025-01-02 15:44:44 +0100

    104 Updated CatalogClient and application.properties with the URL variable

--- src/main/java/ch/fhnw/order/integration/CatalogClient.java
+++ src/main/java/ch/fhnw/order/integration/CatalogClient.java
@@ -1,12 +1,17 @@
 package ch.fhnw.order.integration;
 
 import ch.fhnw.order.web.Book;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 
 @Component
 public class CatalogClient {
     private final RestClient restClient;
+
+    @Value("${catalog.service.url}")
+    private String catalogUrl; 
 
     public CatalogClient(RestClient.Builder restClientBuilder) {
         this.restClient = restClientBuilder.build();
@@ -15,7 +20,7 @@ public class CatalogClient {
     public Book[] findBooks(String search) {
         return restClient
                 .get()
-                .uri("http://catalog:8080/books?search=%s".formatted(search))
+                .uri(catalogUrl + "/books?search=%s".formatted(search))
                 .retrieve()
                 .body(Book[].class);
     }
@@ -23,7 +28,7 @@ public class CatalogClient {
     public Book getBook(String isbn) {
         return restClient
                 .get()
-                .uri("http://catalog:8080/books/%s".formatted(isbn))
+                .uri(catalogUrl + "/books/%s".formatted(isbn))
                 .retrieve()
                 .body(Book.class);
     }
